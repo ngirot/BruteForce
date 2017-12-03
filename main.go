cf1bcfe4b2443c43a4eda6c8723a8f8a72678ffa
Nicolas Girot <ngirot@hotmail.com>
2017-12-03 16:51:06 +0100

    Use flag lic to parse parameters

--- main.go
+++ main.go
@@ -3,26 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s HASH\n", os.Args[0])
-		os.Exit(1)
-	}
+	var bench = flag.Bool("benchmark", false, "Launch a benchmark")
+	var value = flag.String("value", "", "The value to be tested");
+	flag.Parse()
 
-	var hash = os.Args[1]
-	if hash == "--benchmark" {
+	if *bench {
 		fmt.Printf("One CPU hasher : %d kh/s\n", BenchHasher()/1000)
 		fmt.Printf("One CPU word generator : %d kw/s\n", BenchBruter()/1000)
 		os.Exit(0)
+	}
+
+	if *value == "" {
+		flag.Usage();
+		os.Exit(1)
 	}
 
 	fmt.Printf("Start brute-forcing...\n")
 
 	var chrono = NewChrono()
 	chrono.Start()
-	var result = Launch(hash)
+	var result = Launch(*value)
 	chrono.End()
 
 	if result != "" {
