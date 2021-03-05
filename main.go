6dbf0d9be2b6cadca50bc137bc588b9a7dd364b4
Nicolas Girot <ngirot@hotmail.com>
2021-03-05 13:43:49 +0100

    Format code

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func formatBenchResult(number int, unit string) string {
 		return fmt.Sprintf("%d %s", number, unit)
 	}
 	if number < 1000000 {
-		return fmt.Sprintf("%.2f k%s", float64(number) / 1000, unit)
+		return fmt.Sprintf("%.2f k%s", float64(number)/1000, unit)
 	}
-	return fmt.Sprintf("%.2f M%s", float64(number) / 1000000, unit)
+	return fmt.Sprintf("%.2f M%s", float64(number)/1000000, unit)
 }
