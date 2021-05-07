c477212766a89f59e8f0df336b85a937172bc960
Nicolas Girot <ngirot@hotmail.com>
2021-05-08 01:13:24 +0200

    Improve integration tests

--- bruteforce/launcherGpu_test.go
+++ bruteforce/launcherGpu_test.go
@@ -10,64 +10,25 @@ import (
 var gpuConfiguration = conf.NewProcessingUnitConfiguration(true)
 
 func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabet_GPU_IntegrationTest(t *testing.T) {
-	timeout(t, func(t *testing.T) {
-		result, error := Launch(conf.HashConf{
-			Value:    "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data"}, gpuConfiguration)
-
-		checkResult("abc", result, error, t)
-	})
+	runAlphabetTest(t, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad", "sha256", cpuConfiguration)
 }
-
 
 func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltAfter_GPU_IntegrationTest(t *testing.T) {
-	timeout(t, func(t *testing.T) {
-		result, error := Launch(conf.HashConf{
-			Value:    "d43f1b69d87466016e70505bf91c0fd2f075a905b6d18b3bbe2f60ae2ca3dac6",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltAfter: "salty"}, gpuConfiguration)
-
-		checkResult("efg", result, error, t)
-	})
+	runAlphabetSaltAfter(t, "d43f1b69d87466016e70505bf91c0fd2f075a905b6d18b3bbe2f60ae2ca3dac6", "sha256", cpuConfiguration)
 }
 
-
 func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltBefore_GPU_IntegrationTest(t *testing.T) {
-	timeout(t, func(t *testing.T) {
-		result, error := Launch(conf.HashConf{
-			Value:    "73963907b19c38e6cf8d4c9e4c08d966269abd5a17dca0171804be9121bd525c",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltBefore: "salty"}, gpuConfiguration)
-
-		checkResult("efg", result, error, t)
-	})
+	runAlphabetSaltBefore(t, "73963907b19c38e6cf8d4c9e4c08d966269abd5a17dca0171804be9121bd525c", "sha256", cpuConfiguration)
 }
 
 func Test_Launcher_ShouldFindSimpleSha256HashFromDictionary_GPU_IntegrationTest(t *testing.T) {
-	result, error := Launch(conf.HashConf{
-		Value:    "3eb95849b1228d1f28f57e60aa691e78f295245e79681e3bbb7a5807d4e01ed6",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data"}, gpuConfiguration)
-
-	checkResult("fromDic", result, error, t)
+	runDictionary(t, "3eb95849b1228d1f28f57e60aa691e78f295245e79681e3bbb7a5807d4e01ed6", "sha256", cpuConfiguration)
 }
 
 func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltAfter_GPU_IntegrationTest(t *testing.T) {
-	result, error := Launch(conf.HashConf{
-		Value:    "a6c624948b941bd972cf11d58af3cb5bb3ed326eb8107fe7bb5cb4f549706455",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data", SaltAfter: "salty"}, gpuConfiguration)
-
-	checkResult("maybe", result, error, t)
+	runDictionartSaltAfter(t, "a6c624948b941bd972cf11d58af3cb5bb3ed326eb8107fe7bb5cb4f549706455", "sha256", cpuConfiguration)
 }
 
-
 func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltBefore_GPU_IntegrationTest(t *testing.T) {
-	result, error := Launch(conf.HashConf{
-		Value:    "75e1aea225bdb80135b82dce07dfb302a2654bd680113eaf05807017e6a83fe4",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data", SaltBefore: "salty"}, gpuConfiguration)
-
-	checkResult("maybe", result, error, t)
+	runDictionarySaltBefore(t, "75e1aea225bdb80135b82dce07dfb302a2654bd680113eaf05807017e6a83fe4", "sha256", cpuConfiguration)
 }
--- bruteforce/launcher_test.go
+++ bruteforce/launcher_test.go
@@ -10,62 +10,86 @@ type testFunc func(*testing.T)
 
 var cpuConfiguration = conf.NewProcessingUnitConfiguration(false)
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabet_IntegrationTest(t *testing.T) {
+func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabet_CPU_IntegrationTest(t *testing.T) {
+	runAlphabetTest(t, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad", "sha256", cpuConfiguration)
+}
+
+func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltAfter_CPU_IntegrationTest(t *testing.T) {
+	runAlphabetSaltAfter(t, "d43f1b69d87466016e70505bf91c0fd2f075a905b6d18b3bbe2f60ae2ca3dac6", "sha256", cpuConfiguration)
+}
+
+func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltBefore_CPU_IntegrationTest(t *testing.T) {
+	runAlphabetSaltBefore(t, "73963907b19c38e6cf8d4c9e4c08d966269abd5a17dca0171804be9121bd525c", "sha256", cpuConfiguration)
+}
+
+func Test_Launcher_ShouldFindSimpleSha256HashFromDictionary_CPU_IntegrationTest(t *testing.T) {
+	runDictionary(t, "3eb95849b1228d1f28f57e60aa691e78f295245e79681e3bbb7a5807d4e01ed6", "sha256", cpuConfiguration)
+}
+
+func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltAfter_CPU_IntegrationTest(t *testing.T) {
+	runDictionartSaltAfter(t, "a6c624948b941bd972cf11d58af3cb5bb3ed326eb8107fe7bb5cb4f549706455", "sha256", cpuConfiguration)
+}
+
+func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltBefore_CPU_IntegrationTest(t *testing.T) {
+	runDictionarySaltBefore(t, "75e1aea225bdb80135b82dce07dfb302a2654bd680113eaf05807017e6a83fe4", "sha256", cpuConfiguration)
+}
+
+func runAlphabetTest(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	timeout(t, func(t *testing.T) {
 		result, error := Launch(conf.HashConf{
-			Value:    "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data"}, cpuConfiguration)
+			Value:    valueParam,
+			HashType: typeParam,
+		}, conf.WordConf{Alphabet: "testAlphabet.data"}, processUnitConfiguration)
 
 		checkResult("abc", result, error, t)
 	})
 }
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltAfter_IntegrationTest(t *testing.T) {
+func runAlphabetSaltAfter(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	timeout(t, func(t *testing.T) {
 		result, error := Launch(conf.HashConf{
-			Value:    "d43f1b69d87466016e70505bf91c0fd2f075a905b6d18b3bbe2f60ae2ca3dac6",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltAfter: "salty"}, cpuConfiguration)
+			Value:    valueParam,
+			HashType: typeParam,
+		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltAfter: "salty"}, processUnitConfiguration)
 
 		checkResult("efg", result, error, t)
 	})
 }
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromAlphabetWithSaltBefore_IntegrationTest(t *testing.T) {
+func runAlphabetSaltBefore(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	timeout(t, func(t *testing.T) {
 		result, error := Launch(conf.HashConf{
-			Value:    "73963907b19c38e6cf8d4c9e4c08d966269abd5a17dca0171804be9121bd525c",
-			HashType: "sha256",
-		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltBefore: "salty"}, cpuConfiguration)
+			Value:    valueParam,
+			HashType: typeParam,
+		}, conf.WordConf{Alphabet: "testAlphabet.data", SaltBefore: "salty"}, processUnitConfiguration)
 
 		checkResult("efg", result, error, t)
 	})
 }
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromDictionary_IntegrationTest(t *testing.T) {
+func runDictionary(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	result, error := Launch(conf.HashConf{
-		Value:    "3eb95849b1228d1f28f57e60aa691e78f295245e79681e3bbb7a5807d4e01ed6",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data"}, cpuConfiguration)
+		Value:    valueParam,
+		HashType: typeParam,
+	}, conf.WordConf{Dictionary: "testDictionary.data"}, processUnitConfiguration)
 
 	checkResult("fromDic", result, error, t)
 }
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltAfter_IntegrationTest(t *testing.T) {
+func runDictionartSaltAfter(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	result, error := Launch(conf.HashConf{
-		Value:    "a6c624948b941bd972cf11d58af3cb5bb3ed326eb8107fe7bb5cb4f549706455",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data", SaltAfter: "salty"}, cpuConfiguration)
+		Value:    valueParam,
+		HashType: typeParam,
+	}, conf.WordConf{Dictionary: "testDictionary.data", SaltAfter: "salty"}, processUnitConfiguration)
 
 	checkResult("maybe", result, error, t)
 }
 
-func Test_Launcher_ShouldFindSimpleSha256HashFromDictionaryWithSaltBefore_IntegrationTest(t *testing.T) {
+func runDictionarySaltBefore(t *testing.T, valueParam string, typeParam string, processUnitConfiguration conf.ProcessingUnitConfiguration) {
 	result, error := Launch(conf.HashConf{
-		Value:    "75e1aea225bdb80135b82dce07dfb302a2654bd680113eaf05807017e6a83fe4",
-		HashType: "sha256",
-	}, conf.WordConf{Dictionary: "testDictionary.data", SaltBefore: "salty"}, cpuConfiguration)
+		Value:    valueParam,
+		HashType: typeParam,
+	}, conf.WordConf{Dictionary: "testDictionary.data", SaltBefore: "salty"}, processUnitConfiguration)
 
 	checkResult("maybe", result, error, t)
 }
