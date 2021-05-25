4649e154daeff4cf02e077d1ca7cf48d4868c71c
Nicolas Girot <ngirot@hotmail.com>
2021-05-26 00:43:57 +0200

    Optimize GPU unit tests

--- bruteforce/hashs/hashers/gpu/hasherGpuSha1_test.go
+++ bruteforce/hashs/hashers/gpu/hasherGpuSha1_test.go
@@ -8,52 +8,44 @@ import (
 	"testing"
 )
 
+var hasherSha1 = NewHasherGpuSha1()
+
 func TestHasherGpuSha1_Hash_WithSimpleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-	testHashSha1(t, hasher, []string{"test"}, []string{"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"})
+	testHashSha1(t, hasherSha1, []string{"test"}, []string{"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"})
 }
 
 func TestHasherGpuSha1_Hash_WithMultipleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-	testHashSha1(t, hasher,
+	testHashSha1(t, hasherSha1,
 		[]string{"test1", "test2"},
 		[]string{"b444ac06613fc8d63795be9ad0beaf55011936ac", "109f4b3c50d7b0df729d299bc6f8e9ef9066971f"})
 }
 
 func TestHasherGpuSha1_Hash_WithUnicodeWord(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-	testHashSha1(t, hasher, []string{"ありがとう &!ç"}, []string{"cb789c4b10a21cd6fa614384436ac57b0e18c1cd"})
+	testHashSha1(t, hasherSha1, []string{"ありがとう &!ç"}, []string{"cb789c4b10a21cd6fa614384436ac57b0e18c1cd"})
 }
 
 func TestHasherGpuSha1_ProcessWithWildcard_WithSimpleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-
-	var result = hasher.ProcessWithWildcard([]string{"e", "f"}, "", "", 1, "58e6b3a414a1e090dfc6029add0f3555ccba127f")
+	var result = hasherSha1.ProcessWithWildcard([]string{"e", "f"}, "", "", 1, "58e6b3a414a1e090dfc6029add0f3555ccba127f")
 	assertResultSha1(t, result, "e")
 }
 
 func TestHasherGpuSha1_ProcessWithWildcard_WithSaltBefore(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-
-	var result = hasher.ProcessWithWildcard([]string{"d", "e"}, "t", "", 1, "33e9505d12942e8259a3c96fb6f88ed325b95797")
+	var result = hasherSha1.ProcessWithWildcard([]string{"d", "e"}, "t", "", 1, "33e9505d12942e8259a3c96fb6f88ed325b95797")
 	assertResultSha1(t, result, "e")
 }
 
 func TestHasherGpuSha1_ProcessWithWildcard_WithSaltAfter(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
-
-	var result = hasher.ProcessWithWildcard([]string{"d", "e", "f"}, "", "t", 1, "d352dbdf6170085acaf7ed62197a4de1452a0073")
+	var result = hasherSha1.ProcessWithWildcard([]string{"d", "e", "f"}, "", "t", 1, "d352dbdf6170085acaf7ed62197a4de1452a0073")
 	assertResultSha1(t, result, "f")
 }
 
 func TestHasherGpuSha1_Hash_ConsistencyWithSameHash(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
 	var testString = "test"
 
-	var firstResult = sha1ToString(hasher.Hash([]string{testString})[0])
+	var firstResult = sha1ToString(hasherSha1.Hash([]string{testString})[0])
 
 	for i := 0; i < 10; i++ {
-		var anotherResult = sha1ToString(hasher.Hash([]string{testString})[0])
+		var anotherResult = sha1ToString(hasherSha1.Hash([]string{testString})[0])
 		if anotherResult != firstResult {
 			t.Errorf("Hasher is not consistent : the first value was '%s', but it another all returned '%s'", firstResult, anotherResult)
 		}
@@ -61,28 +53,25 @@ func TestHasherGpuSha1_Hash_ConsistencyWithSameHash(t *testing.T) {
 }
 
 func TestHasherGpuSha1_IsValid_WithAValidHash(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
 	var hash = "cb789c4b10a21cd6fa614384436ac57b0e18c1cd"
 
-	if !hasher.IsValid(hash) {
+	if !hasherSha1.IsValid(hash) {
 		t.Errorf("The hash '%s' should be valid", hash)
 	}
 }
 
 func TestHasherGpuSha1_IsValid_WithAValueWithIncorrectSize(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
 	var hash = "cb789c4b10a21cd6fa614384436ac57b0e"
 
-	if hasher.IsValid(hash) {
+	if hasherSha1.IsValid(hash) {
 		t.Errorf("The hash '%s' should not be valid", hash)
 	}
 }
 
 func TestHasherGpuSha1_IsValid_WithAValueWithNotvalidBase64Char(t *testing.T) {
-	var hasher = NewHasherGpuSha1()
 	var hash = "cb789c4b10a21µd6fa614384436ac57b0e18c1cd"
 
-	if hasher.IsValid(hash) {
+	if hasherSha1.IsValid(hash) {
 		t.Errorf("The hash '%s' should not be valid", hash)
 	}
 }
--- bruteforce/hashs/hashers/gpu/hasherGpuSha256_test.go
+++ bruteforce/hashs/hashers/gpu/hasherGpuSha256_test.go
@@ -8,52 +8,44 @@ import (
 	"testing"
 )
 
+var hasherSha256 = NewHasherGpuSha256()
+
 func TestHasherGpuSha256_Hash_WithSimpleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-	testHashGpuSha256(t, hasher, []string{"test"}, []string{"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"})
+	testHashGpuSha256(t, hasherSha256, []string{"test"}, []string{"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"})
 }
 
 func TestHasherGpuSha256_Hash_WithMultipleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-	testHashGpuSha256(t, hasher,
+	testHashGpuSha256(t, hasherSha256,
 		[]string{"test1", "test2"},
 		[]string{"1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014", "60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752"})
 }
 
 func TestHasherGpuSha256_Hash_WithUnicodeWord(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-	testHashGpuSha256(t, hasher, []string{"ありがとう &!ç"}, []string{"f89eddccb44ae418616060aefe3ca6604d49bc3d0e37e75167333d498532d7aa"})
+	testHashGpuSha256(t, hasherSha256, []string{"ありがとう &!ç"}, []string{"f89eddccb44ae418616060aefe3ca6604d49bc3d0e37e75167333d498532d7aa"})
 }
 
 func TestHasherGpuSha256_ProcessWithWildcard_WithSimpleWord(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-
-	var result = hasher.ProcessWithWildcard([]string{"e", "f"}, "", "", 1, "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea.Gt4wp0dJk5qWRaumcfqazMMCAxxerGi")
+	var result = hasherSha256.ProcessWithWildcard([]string{"e", "f"}, "", "", 1, "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea.Gt4wp0dJk5qWRaumcfqazMMCAxxerGi")
 	assertResultGpuSha256(t, result, "e")
 }
 
 func TestHasherGpuSha256_ProcessWithWildcard_WithSaltBefore(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-
-	var result = hasher.ProcessWithWildcard([]string{"d", "e"}, "t", "", 1, "2d6c9a90dd38f6852515274cde41a8cd8e7e1a7a053835334ec7e29f61b918dd")
+	var result = hasherSha256.ProcessWithWildcard([]string{"d", "e"}, "t", "", 1, "2d6c9a90dd38f6852515274cde41a8cd8e7e1a7a053835334ec7e29f61b918dd")
 	assertResultGpuSha256(t, result, "e")
 }
 
 func TestHasherGpuSha256_ProcessWithWildcard_WithSaltAfter(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
-
-	var result = hasher.ProcessWithWildcard([]string{"d", "e", "f"}, "", "t", 1, "9adf009b9ea42792e73e5dc86e9c9024489d74ad894c0a3acbc03095158eaabc")
+	var result = hasherSha256.ProcessWithWildcard([]string{"d", "e", "f"}, "", "t", 1, "9adf009b9ea42792e73e5dc86e9c9024489d74ad894c0a3acbc03095158eaabc")
 	assertResultGpuSha256(t, result, "f")
 }
 
 func TestHasherGpuSha256_Hash_ConsistencyWithSameHash(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
 	var testString = "test"
 
-	var firstResult = gpuSha256ToString(hasher.Hash([]string{testString})[0])
+	var firstResult = gpuSha256ToString(hasherSha256.Hash([]string{testString})[0])
 
 	for i := 0; i < 10; i++ {
-		var anotherResult = gpuSha256ToString(hasher.Hash([]string{testString})[0])
+		var anotherResult = gpuSha256ToString(hasherSha256.Hash([]string{testString})[0])
 		if anotherResult != firstResult {
 			t.Errorf("Hasher is not consistent : the first value was '%s', but it another all returned '%s'", firstResult, anotherResult)
 		}
@@ -61,28 +53,25 @@ func TestHasherGpuSha256_Hash_ConsistencyWithSameHash(t *testing.T) {
 }
 
 func TestHasherGpuSha256_IsValid_WithAValidHash(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
 	var hash = "f89eddccb44ae418616060aefe3ca6604d49bc3d0e37e75167333d498532d7aa"
 
-	if !hasher.IsValid(hash) {
+	if !hasherSha256.IsValid(hash) {
 		t.Errorf("The hash '%s' should be valid", hash)
 	}
 }
 
 func TestHasherGpuSha256_IsValid_WithAValueWithIncorrectSize(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
 	var hash = "f89eddccb44ae418616060aefe3ca6604d49bc3d0e37e7516733"
 
-	if hasher.IsValid(hash) {
+	if hasherSha256.IsValid(hash) {
 		t.Errorf("The hash '%s' should not be valid", hash)
 	}
 }
 
 func TestHasherGpuSha256_IsValid_WithAValueWithNotvalidBase64Char(t *testing.T) {
-	var hasher = NewHasherGpuSha256()
 	var hash = "f89eddccb44ae41:616060aefe3ca6604d49bc3d0e37e75167333d498532d7aa"
 
-	if hasher.IsValid(hash) {
+	if hasherSha256.IsValid(hash) {
 		t.Errorf("The hash '%s' should not be valid", hash)
 	}
 }
